e729fc7775579f67214a8798b8e5bf21b87faf06
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-06-20 16:04:24 +0100

    Test complete

--- src/index.js
+++ src/index.js
@@ -1,4 +1,37 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
+
+    const ids = []
+    fetch('http://localhost:3000/movies')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(element => {
+            ids.push(element.id)   
+        });
+        
+    })
+
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        
+        //let userSearch = event.target.children[1].value
+        const input = document.querySelector('input#searchByID');
+        let userSearch = input.value
+        
+
+        if (ids.includes(parseInt(userSearch))){
+            fetch(`http://localhost:3000/movies/${userSearch}`)
+            .then(response => response.json())
+            .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+            title.innerText = data.title;
+            summary.innerText = data.summary;})
+        }else{
+            alert(`${userSearch} is not a valid id`)
+        }
+
+    });
   
 }
 
